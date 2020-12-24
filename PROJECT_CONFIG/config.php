9f9994e8bdc035bd34ae66b2c156df0f26992be8
Soumyajit Datta <64341057+Codehackerone@users.noreply.github.com>
2020-12-24 16:32:31 +0530

    Update config.php

--- PROJECT_CONFIG/config.php
+++ PROJECT_CONFIG/config.php
@@ -1,8 +1,8 @@
 <?php
 	$host = "localhost";
-    $dbUsername = "root";
+    $dbUsername = "";
     $dbPassword = "";
-    $dbname = "sales1";
+    $dbname = "";
 		$conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
 		if (mysqli_connect_error())
 		 {
